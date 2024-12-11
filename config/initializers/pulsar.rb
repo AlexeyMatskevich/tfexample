38bbb0fb7229aced3a1b2bcbe524bcffa688b380
Alexey <github_job@mackevich.addymail.com>
2024-12-11 22:11:29 +0500

    Fix bug when consumer created twice on dip up

--- config/initializers/pulsar.rb
+++ config/initializers/pulsar.rb
@@ -1,19 +1,19 @@
-unless ENV['SKIP_PULSAR']
-  Java.add_to_classpath("/app/pulsar-java-client/lib/build/libs/lib-all.jar")
-
-  module Pulsar
-    Pulsar = Java.type('org.apache.pulsar.client.api.PulsarClient') # Get pulsar client
-    Schema = Java.type('org.apache.pulsar.client.api.Schema')
-    Client = Pulsar.builder.serviceUrl(ENV["PULSAR_URL"]).build # Greate client
-    TestProducer = Client
-                     .newProducer(Schema['STRING'])
-                     .topic("my-topic")
-                     .create
-
-    # TestConsumer = Client
-    #                  .newConsumer(Schema['STRING'])
-    #                  .topic("my-topic")
-    #                  .subscriptionName("my-subscription")
-    #                  .subscribe
-  end
-end
+# unless ENV['SKIP_PULSAR']
+#   Java.add_to_classpath("/app/pulsar-java-client/lib/build/libs/lib-all.jar")
+#
+#   module Pulsar
+#     Pulsar = Java.type('org.apache.pulsar.client.api.PulsarClient') # Get pulsar client
+#     Schema = Java.type('org.apache.pulsar.client.api.Schema')
+#     Client = Pulsar.builder.serviceUrl(ENV["PULSAR_URL"]).build # Greate client
+#     TestProducer = Client
+#                      .newProducer(Schema['STRING'])
+#                      .topic("my-topic")
+#                      .create
+#
+#     # TestConsumer = Client
+#     #                  .newConsumer(Schema['STRING'])
+#     #                  .topic("my-topic")
+#     #                  .subscriptionName("my-subscription")
+#     #                  .subscribe
+#   end
+# end
